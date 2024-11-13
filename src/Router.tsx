6a2d65a557e1f90b435774c8ae76ec2160db0bcf
山口 海斗 <k-yamaguchi@ies.local>
2024-11-13 16:39:27 +0900

    Fix: ThemeProviderの実装先を修正

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,10 +1,14 @@
 import { BrowserRouter } from 'react-router-dom';
 import { Layout } from './components';
+import { ThemeProvider } from '@mui/material';
+import { theme } from './features';
 
 function AppRoutes() {
 	return (
 		<BrowserRouter basename={import.meta.env.VITE_BASE || ''}>
-			<Layout />
+			<ThemeProvider theme={theme}>
+				<Layout />
+			</ThemeProvider>
 		</BrowserRouter>
 	);
 }
--- src/main.tsx
+++ src/main.tsx
@@ -4,14 +4,10 @@ import './index.css';
 import './App.css';
 import { Axios } from './hooks';
 import AppRoutes from './Router';
-import { theme } from './features';
-import { ThemeProvider } from '@mui/material';
 
 createRoot(document.getElementById('root')!).render(
 	<StrictMode>
 		<Axios />
-		<ThemeProvider theme={theme}>
-			<AppRoutes />
-		</ThemeProvider>
+		<AppRoutes />
 	</StrictMode>
 );
